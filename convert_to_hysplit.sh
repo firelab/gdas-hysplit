0401d426f1e57d635c3b6dcbb5f59ab3676158b9
Bryce Nordgren <bryce.l.nordgren@usda.gov>
2020-01-16 13:47:31 -0700

    updated conversion script to june.

--- convert_to_hysplit.sh
+++ convert_to_hysplit.sh
@@ -2,10 +2,10 @@
 
 cnv=/opt/hysplit4/exec/api2arl_v4
 #last_file=
-last_file=ncep_global_ssi.2017052818/gdas1.t18z.pgrb2.0p25.f005
+last_file=ncep_global_ssi.2017062518/gdas1.t18z.pgrb2.0p25.f005
 
-for d_i in $(seq -f "%02.f" 28 31) ; do 
-    for d in $(ls -d ncep_global_ssi.201705${d_i}??) ; do
+for d_i in $(seq -f "%02.f" 26 30) ; do 
+    for d in $(ls -d ncep_global_ssi.201706${d_i}??) ; do
         timestamp=$(echo $d | cut -c 17-) 
         for i_f in {0..5} ; do 
             f=$(ls $d/gdas1.t??z.pgrb2.0p25.f00$i_f)
